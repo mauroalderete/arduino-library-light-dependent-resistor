a01e3b346b18e157c3ac115e2e2524f0c153e7f6
alderete.mauro <alderete.mauro@gmail.com>
2020-10-11 19:58:38 -0300

    incorporo comentarios

--- src/LightDependentResistor.h
+++ src/LightDependentResistor.h
@@ -4,28 +4,74 @@
 #include <Arduino.h>
 #include <EventHandler.h>
 
+/**
+* @brief Simplifica la interacción con un sensor LDR en modo ditigal
+* */
 class LightDependentResistor{
     private:
         uint8_t _pin;
         bool _value;
 
     public:
+        /**
+        * @brief Evento que se dispara cuando se detecta un cambio de estado luego de ejecutar Read()
+        * */
         EventHandler<bool> Changed;
 
+        /**
+        * @brief Instancia e inicializa el objeto sin valores configurados
+        * */
         LightDependentResistor();
+        /**
+        * @brief Instancia e inicializa el objeto con un pin, pero sin callback
+        * @param uint8_t Pin al que esta conectado el sensor LDR
+        * */
         LightDependentResistor(uint8_t pin);
+        /**
+        * @brief Instancia e inicializa el objeto con un pin y un callback asociado al evento Changed
+        * @param uint8_t Pin al que esta conectado el sensor LDR
+        * @param void (*onChanged)(bool) Callback que se invocara cuando se detecte un cambio de estado durante la ejecución de Read()
+        * */
         LightDependentResistor(uint8_t pin, void (*onChanged)(bool));
+        /**
+        * @brief Destruye toda referencia al evento Changed
+        * */
         ~LightDependentResistor();
 
+        /**
+        * @brief Inicializa el objeto sin valores configurados
+        * */
         void Setup();
+        /**
+        * @brief Inicializa el objeto con un pin, pero sin callback
+        * @param uint8_t Pin al que esta conectado el sensor LDR
+        * */
         void Setup(uint8_t pin);
+        /**
+        * @brief Inicializa el objeto con un pin y un callback asociado al evento Changed
+        * @param uint8_t Pin al que esta conectado el sensor LDR
+        * @param void (*onChanged)(bool) Callback que se invocara cuando se detecte un cambio de estado durante la ejecución de Read()
+        * */
         void Setup(uint8_t pin, void (*onChanged)(bool));
 
+        /**
+        * @brief Cambia el pin de lectura
+        * @param uint8_t Pin al que esta conectado el sensor LDR
+        * */
         void SetPin(uint8_t pin);
+        /**
+        * @brief Devuelve el pin de lectura actual
+        * */
         uint8_t GetPin();
         
+        /**
+        * @brief Devuelve el ultimo valor leido
+        * */
         bool GetValue();
 
+        /**
+        * @brief Lee y devuelve el estado actual del LDR, si hubo un cambio dispara el evento Changed
+        * */
         bool Read();
 };
 
